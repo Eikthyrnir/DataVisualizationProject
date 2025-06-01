44ac848acf8c5159f61a408fd3901d6ed98f94d1
Eikthyrnir <tim.barvenov@gmail.com>
2025-06-01 20:51:51 +0200

    added map coloring for Frekwencja

--- map.js
+++ map.js
@@ -17,19 +17,7 @@ function colorMapRegions() {
   const subregionType = getSubregionType(selectedRegionType);
   if (!layers[subregionType]) return;
 
-  const frekwencjiByLayer = {}
-  let maxFreq = 0.0;
-  let minFreq = 1.0;
-  layers[subregionType].eachLayer(l => {
-    const region = l.feature.properties;
-    const regionNameForFiltering = getRegionNameForFiltering(region);
-
-    const {uprawnionych, wydano} = frekwencjaPerRegion[subregionType][regionNameForFiltering];
-    const freq = uprawnionych > 0 ? wydano / uprawnionych : 0;
-    frekwencjiByLayer[regionNameForFiltering] = freq;
-    maxFreq = Math.max(maxFreq, freq);
-    minFreq = Math.min(minFreq, freq);
-  })
+  const {frekwencjiByLayer, minFreq, maxFreq} = getFrekwencja(layers['woj'].getLayers().map((l) => l.feature));
 
   layers[subregionType].eachLayer(layer => {
     let region = layer.feature.properties;
@@ -157,100 +145,129 @@ fetch('contours_data/WGS84/gminy.json')
     regionData['gmina'] = data;
   });
 
+function getFrekwencja(layerFeatures) {
+  const frekwencjiByLayer = {}
+  let maxFreq = 0.0;
+  let minFreq = 1.0;
+  layerFeatures.forEach(f => {
+    const region = f.properties;
+    const regionNameForFiltering = getRegionNameForFiltering(region);
+
+    const {uprawnionych, wydano} = frekwencjaPerRegion[getRegionType(region)][regionNameForFiltering];
+    const freq = uprawnionych > 0 ? wydano / uprawnionych : 0;
+    frekwencjiByLayer[regionNameForFiltering] = freq;
+    maxFreq = Math.max(maxFreq, freq);
+    minFreq = Math.min(minFreq, freq);
+  })
+  return {frekwencjiByLayer, minFreq, maxFreq};
+}
+
 fetch('contours_data/pol_admbnda_adm1_gov_v02_20220414.json')
   .then(res => res.json())
   .then(data => {
     regionData['woj'] = data;
 
-    function highlightFeature(e) {
-      var layer = e.target;
-      layer.setStyle({
-        fillColor: "#a10028",
-        fillOpacity: 1
-      });
-
-      // Get center of the region (bounds center is simple and works well for Poland)
-      var center = layer.getBounds().getCenter();
-
-      const props = layer.feature.properties;
-
-      // Add custom tooltip at center
-      layer.bindTooltip(
-        getRegionName(props),
-        {
-          permanent: true,
-          direction: 'center',
-          className: 'custom-tooltip',
-          offset: [0,0],
-          interactive: false
-        }
-      ).openTooltip(center);
-    }
-
-    function resetHighlight(e, layer) {
-      layer.resetStyle(e.target);
-      e.target.unbindTooltip();
-    }
-
-    function zoomToFeature(e) {
-      selectedRegion = e.target.feature.properties;
-      selectedRegionType = getRegionType(selectedRegion);
-      map.fitBounds(e.target.getBounds());
-
-      // Select in dropdown:
-      regionSelector[selectedRegionType].value = getRegionNameForFiltering(selectedRegion)
-      regionSelector[selectedRegionType].dispatchEvent(new Event('change'));
-
-      if (layers['powiat']) {
-        map.removeLayer(layers['powiat']);
-      }
-      if (layers['gmina']) {
-        map.removeLayer(layers['gmina']);
-      }
-
-      const subregionType = getSubregionType(selectedRegionType)
-      const selectedRegionId = getRegionId(selectedRegion)
-      const filteredSubregions = {
-        ...regionData[subregionType],
-        features: regionData[subregionType].features.filter(f =>
-          (f.properties.JPT_KOD_JE.startsWith(selectedRegionId))
-        )
-      };
-
-      nextLayer = L.geoJson(filteredSubregions, {
-        style: {
-          color: "#0066cc",
-          weight: 1.5,
-          fillColor: "#aad3df",
-          fillOpacity: 0.6
-        },
-        onEachFeature: function(feature, layer) {
-          layer.on({
-            mouseover: highlightFeature,
-            mouseout: (e) => resetHighlight(e, nextLayer),
-            click: zoomToFeature
-          });
-        }
-      }).addTo(map);
-
-      layers[subregionType] = nextLayer;
-    }
-
-    function onEachFeature(feature, layer) {
-      layer.on({
-        mouseover: highlightFeature,
-        mouseout: (e) => resetHighlight(e, layers['woj']),
-        click: zoomToFeature
-      });
-    }
+    const {frekwencjiByLayer, minFreq, maxFreq} = getFrekwencja(data.features);
 
     layers['woj'] = L.geoJson(data, {
-      style: {
+      style: (f) => ({
         color: "#999",
         weight: 1.5,
-        fillColor: "#dddddd",
+        fillColor: layerStylingFunction(f, "#dddddd", frekwencjiByLayer, minFreq, maxFreq),
         fillOpacity: 0.6
-      },
+      }),
       onEachFeature: onEachFeature
     }).addTo(map);
   });
+
+function highlightFeature(e) {
+  var layer = e.target;
+  layer.setStyle({
+    fillColor: "#a10028",
+    fillOpacity: 1
+  });
+
+  // Get center of the region (bounds center is simple and works well for Poland)
+  var center = layer.getBounds().getCenter();
+
+  const props = layer.feature.properties;
+
+  // Add custom tooltip at center
+  layer.bindTooltip(
+    getRegionName(props),
+    {
+      permanent: true,
+      direction: 'center',
+      className: 'custom-tooltip',
+      offset: [0,0],
+      interactive: false
+    }
+  ).openTooltip(center);
+}
+
+function resetHighlight(e, layer) {
+  layer.resetStyle(e.target);
+  e.target.unbindTooltip();
+}
+
+function zoomToFeature(e) {
+  selectedRegion = e.target.feature.properties;
+  selectedRegionType = getRegionType(selectedRegion);
+  map.fitBounds(e.target.getBounds());
+
+  // Select in dropdown:
+  regionSelector[selectedRegionType].value = getRegionNameForFiltering(selectedRegion)
+  regionSelector[selectedRegionType].dispatchEvent(new Event('change'));
+
+  if (layers['powiat']) {
+    map.removeLayer(layers['powiat']);
+  }
+  if (layers['gmina']) {
+    map.removeLayer(layers['gmina']);
+  }
+
+  const subregionType = getSubregionType(selectedRegionType)
+  const selectedRegionId = getRegionId(selectedRegion)
+  const filteredSubregions = {
+    ...regionData[subregionType],
+    features: regionData[subregionType].features.filter(f =>
+      (f.properties.JPT_KOD_JE.startsWith(selectedRegionId))
+    )
+  };
+
+
+  const {frekwencjiByLayer, minFreq, maxFreq} = getFrekwencja(filteredSubregions.features);
+
+  nextLayer = L.geoJson(filteredSubregions, {
+    style: (f) => ({
+      color: "#0066cc",
+      weight: 1.5,
+      fillColor: layerStylingFunction(f, "#aad3df", frekwencjiByLayer, minFreq, maxFreq),
+      fillOpacity: 0.6
+    }),
+    onEachFeature: function(feature, layer) {
+      layer.on({
+        mouseover: highlightFeature,
+        mouseout: (e) => resetHighlight(e, nextLayer),
+        click: zoomToFeature
+      });
+    }
+  }).addTo(map);
+
+  layers[subregionType] = nextLayer;
+}
+
+function layerStylingFunction(feature, baseColor, frekwencjiByLayer, minFreq, maxFreq) {
+  if (currentMode === 'frekwencja') {
+    return getFreqColor(frekwencjiByLayer[getRegionNameForFiltering(feature.properties)], minFreq, maxFreq);
+  }
+  return baseColor;
+}
+
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: (e) => resetHighlight(e, layers['woj']),
+    click: zoomToFeature
+  });
+}
